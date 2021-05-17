8fdfbe4bf250f7aa5d46ab808a0c761543782442
ryoha000 <the7uya@gmail.com>
2021-05-17 20:25:11 +0900

    add test of get_around_indexes and fix self

--- src/divide/utils.rs
+++ src/divide/utils.rs
@@ -1,23 +1,46 @@
 pub fn get_around_indexes(index: u32, binary: &(u32, u32, Vec<u32>)) -> Vec<u32> {
     // TODO: ここ一々配列をアロケートしてるのをなんとかする
     let mut result = Vec::new();
-    let top = index - binary.0;
     let bottom = index + binary.0;
-    let left = index - 1;
     let right = index + 1;
 
     // ちゃんとindex内にあるかチェック
-    if top >= 0 {
-        result.push(top)
+    if index >= binary.0 {
+        result.push(index - binary.0);
     }
     if bottom < binary.1 * binary.0 {
-        result.push(bottom)
+        result.push(bottom);
     }
-    if left % binary.0 != 0 && left >= 0 {
-        result.push(left)
+    if index >= 1 {
+        let left = index - 1;
+        if left % binary.0 != 0 {
+            result.push(left);
+        }
     }
     if right % binary.0 != 0 && right < binary.1 * binary.0 {
-        result.push(right)
+        result.push(right);
     }
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils::test;
+    #[test]
+    fn can_get_correct_indexes_of_edge() {
+        let binary = (10, 10, vec![]);
+        
+        let indexes = get_around_indexes(0, &binary);
+        let correct = vec![10, 1];
+        assert!(test::cmp_vec(&indexes, &correct), "indexes({:?}) not equal {:?}", indexes, correct);
+    }
+    #[test]
+    fn can_get_around_indexes_of_center() {
+        let binary = (10, 10, vec![]);
+
+        let indexes = get_around_indexes(55, &binary);
+        let correct = vec![45, 65, 54, 56];
+        assert!(test::cmp_vec(&indexes, &correct), "indexes({:?}) not equal {:?}", indexes, correct);
+    }
 }
