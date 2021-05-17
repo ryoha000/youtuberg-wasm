30e3e04795d01ab20bc04f88f9fcfd88fde15587
ryoha000 <the7uya@gmail.com>
2021-05-17 21:02:23 +0900

    fix divide/utils

--- src/divide/utils.rs
+++ src/divide/utils.rs
@@ -1,4 +1,4 @@
-pub fn get_around_indexes(index: u32, binary: &(u32, u32, Vec<u32>)) -> Vec<u32> {
+pub fn get_around_indexes(index: u32, binary: &(u32, u32, Vec<bool>)) -> Vec<usize> {
     // TODO: ここ一々配列をアロケートしてるのをなんとかする
     let mut result = Vec::new();
     let bottom = index + binary.0;
@@ -6,19 +6,19 @@ pub fn get_around_indexes(index: u32, binary: &(u32, u32, Vec<u32>)) -> Vec<u32>
 
     // ちゃんとindex内にあるかチェック
     if index >= binary.0 {
-        result.push(index - binary.0);
+        result.push((index - binary.0) as usize);
     }
     if bottom < binary.1 * binary.0 {
-        result.push(bottom);
+        result.push(bottom as usize);
     }
     if index >= 1 {
         let left = index - 1;
         if left % binary.0 != 0 {
-            result.push(left);
+            result.push(left as usize);
         }
     }
     if right % binary.0 != 0 && right < binary.1 * binary.0 {
-        result.push(right);
+        result.push(right as usize);
     }
     result
 }
