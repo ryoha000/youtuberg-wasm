71265cf6f1faef8ac2a0e6317a9093817fcce396
ryoha000 <the7uya@gmail.com>
2021-05-17 17:35:23 +0900

    fix histgram

--- src/threshold/histgram.rs
+++ src/threshold/histgram.rs
@@ -1,12 +1,12 @@
-pub fn get_hist(gray: &[u8]) -> [u8; 256] {
-    let mut hist: [u8; 256] = [0; 256];
-    for i in gray.iter() {
+pub fn get_hist(gray: &Vec<u8>) -> [u32; 256] {
+    let mut hist: [u32; 256] = [0; 256];
+    for i in gray {
         hist[*i as usize] += 1;
     }
     hist
 }
 
-pub fn get_threshold_by_hist(hist: &[u8; 256]) -> u8 {
+pub fn get_threshold_by_hist(hist: &[u32; 256]) -> u8 {
     // 元画像の要素の合計
     let mut number_sum: u32 = 0;
     // 画素値の合計
@@ -25,19 +25,28 @@ pub fn get_threshold_by_hist(hist: &[u8; 256]) -> u8 {
         }
 
         let mu2: u32;
-        match next.0 {
+        match next.1 {
             0 => mu2 = 0,
-            length => mu2 = next.2 / length
+            length => mu2 = next.3 / length
         }
 
-        let s = (next.0 * next.1 * (mu1 - mu2)).pow(2);
+        let diff: u32 = 
+            if mu1 > mu2 {
+                mu1 - mu2
+            } else {
+                mu2 - mu1
+            };
+        let s: u64 = next.0 as u64 * next.1 as u64 * diff.pow(2) as u64;
 
         if max_s_variance.1 < s {
             max_s_variance = (i, s);
         }
 
-        let next_hist = hist[i as usize + 1] as u32;
-        next = (next.0 + next_hist, next.1 - next_hist, next.2 + next_hist * (i as u32 + 1), next.3 - next_hist * (i as u32 - 1));
+        if i == 255 {
+            break
+        }
+        let next_hist = hist[i as usize] as u32;
+        next = (next.0 + next_hist, next.1 - next_hist, next.2 + next_hist * (i as u32), next.3 - next_hist * (i as u32));
     }
     max_s_variance.0
 }
@@ -47,7 +56,7 @@ mod tests {
     use super::*;
     #[test]
     fn get_hist_works() {
-        let gray = [100, 0, 1, 2, 4, 100, 4, 100];
+        let gray = vec![100, 0, 1, 2, 4, 100, 4, 100];
         let hist = get_hist(&gray);
         for i in 0..256 {
             match i {
