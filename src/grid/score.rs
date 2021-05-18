6de36c5848cbb2f4dc2869c2ed8847ef273c4bb9
ryoha000 <the7uya@gmail.com>
2021-05-18 19:17:12 +0900

    add test of get_grid_scores

--- src/grid/score.rs
+++ src/grid/score.rs
@@ -17,3 +17,55 @@ pub fn get_grid_scores(binary: &(u32, u32, Vec<bool>), grid: &Grid, side: usize)
     }
     return result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn all_0_scores() {
+        let binary = (5, 3, vec![
+            false,false,false,false,false,
+            false,false,false,false,false,
+            false,false,false,false,false,
+        ]);
+        let side = 1;
+        let grid = Grid{ rows: (binary.1 as f32 / side as f32).ceil() as usize, cols: (binary.0 as f32 / side as f32).ceil() as usize};
+        let scores = get_grid_scores(&binary, &grid, side);
+        for score in scores {
+            assert_eq!(score, 0)
+        }
+    }
+
+    #[test]
+    fn all_1_scores() {
+        let binary = (5, 3, vec![
+            true,true,true,true,true,
+            true,true,true,true,true,
+            true,true,true,true,true,
+        ]);
+        let side = 1;
+        let grid = Grid{ rows: (binary.1 as f32 / side as f32).ceil() as usize, cols: (binary.0 as f32 / side as f32).ceil() as usize};
+        let scores = get_grid_scores(&binary, &grid, side);
+        for score in scores {
+            assert_eq!(score, 1)
+        }
+    }
+
+    #[test]
+    fn can_get_surplus_scores() {
+        let binary = (5, 3, vec![
+            true,   true,   true,   false,  true,
+            true,   false,  false,  true,   false,
+            false,  false,  true,   false,  true,
+        ]);
+        let side = 2;
+        let grid = Grid{ rows: (binary.1 as f32 / side as f32).ceil() as usize, cols: (binary.0 as f32 / side as f32).ceil() as usize};
+        let scores = get_grid_scores(&binary, &grid, side);
+        assert_eq!(scores[0], 3);
+        assert_eq!(scores[1], 2);
+        assert_eq!(scores[2], 1);
+        assert_eq!(scores[3], 0);
+        assert_eq!(scores[4], 1);
+        assert_eq!(scores[5], 1);
+    }
+}
