c8d80f3c5bce97fc1d8eb846490608a4414b85cd
ryoha000 <the7uya@gmail.com>
2021-05-19 20:31:01 +0900

    add main method

---
+++ src/grid/mod.rs
@@ -0,0 +1,15 @@
+mod score;
+mod label;
+mod merge;
+
+use crate::utils;
+
+pub fn get_labels(width: u32, height: u32, binary: &[bool]) -> Vec<u32> {
+    let side = width / 40;
+    let grid = utils::Grid{ rows: (height as f32 / side as f32).ceil() as usize, cols: (width as f32 / side as f32).ceil() as usize };
+    let scores = score::get_grid_scores(&(width, height, binary), &grid, side as usize);
+    let mut grid_labels = label::get_labels_by_scores(&scores, &grid, side * side / 10);
+    merge::get_merged_grid_labels(&mut grid_labels, &scores, &grid, 0);
+    merge::fill_missing_grid(&mut grid_labels.labels, &grid);
+    grid_labels.labels
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod threshold;
 mod divide;
 mod utils;
 mod filter;
+mod grid;
 
 #[wasm_bindgen]
 extern {
@@ -14,4 +15,12 @@ extern {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+fn get_labels_by_gray(gray: &[u8], width: u32, height: u32) -> Vec<u32> {
+    let binary = crate::threshold::gray_to_binary(gray);
+    let divided_binary = divide::get_divided_binary(&(width, height, &binary));
+    let noise_filtered_binary = filter::get_filtered_binary(&divided_binary, width / 40);
+    let labels = grid::get_labels(width, height, &noise_filtered_binary);
+    labels
 }
