e8256d6948fa28db452e95576840a3a7e2dd20ca
ryoha000 <the7uya@gmail.com>
2021-05-19 20:20:24 +0900

    fix utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,7 +7,6 @@ pub struct GridLabel {
 
 pub struct DividedBinary {
     pub labels: Vec<u32>,
-    pub contrours: Vec<bool>,
     pub areas: Vec<u32>,
     pub sizes: Vec<DividedSize>,
 }
@@ -42,11 +41,11 @@ pub fn get_around_indexes(index: usize, size: &ImageSize) -> Vec<usize> {
     }
     if index >= 1 {
         let left = index - 1;
-        if left % size.width != 0 {
+        if left % size.width != size.width - 1 {
             result.push(left as usize);
         }
     }
-    if right % size.width != 0 && right < size.height * size.width {
+    if right % size.width != size.width - 1 && right < size.height * size.width {
         result.push(right as usize);
     }
     result
@@ -57,7 +56,7 @@ pub fn get_contrours_from_labels(labels: &[u32], size: &ImageSize) -> Vec<bool> 
     let mut contrours = vec![false; labels.len()];
     for i in 0..labels.len() {
         contrours[i] = false;
-        if labels[i] == 0 {
+        if labels[i] == 1 {
             continue
         }
         let indexes = get_around_indexes(i, size);
@@ -66,7 +65,7 @@ pub fn get_contrours_from_labels(labels: &[u32], size: &ImageSize) -> Vec<bool> 
         }
         let mut is_edge = false;
         for j in 0..indexes.len() {
-            if labels[indexes[j]] == 0 {
+            if labels[indexes[j]] == 1 {
                 is_edge = true;
                 break;
             }
@@ -83,15 +82,19 @@ mod tests {
     use super::*;
     use crate::utils::test;
     #[test]
-    fn can_get_correct_indexes_of_edge() {        
+    fn can_get_correct_indexes_of_left_top_edge() {        
         let indexes = get_around_indexes(0, &ImageSize{ width: 10, height: 10 });
         let correct = vec![10, 1];
         assert!(test::cmp_vec(&indexes, &correct), "indexes({:?}) not equal {:?}", indexes, correct);
     }
     #[test]
+    fn can_get_correct_indexes_of_left_center() {        
+        let indexes = get_around_indexes(11, &ImageSize{ width: 10, height: 10 });
+        let correct = vec![1, 21, 10, 12];
+        assert!(test::cmp_vec(&indexes, &correct), "indexes({:?}) not equal {:?}", indexes, correct);
+    }
+    #[test]
     fn can_get_around_indexes_of_center() {
-        let binary = (10, 10, vec![]);
-
         let indexes = get_around_indexes(55, &ImageSize{ width: 10, height: 10 });
         let correct = vec![45, 65, 54, 56];
         assert!(test::cmp_vec(&indexes, &correct), "indexes({:?}) not equal {:?}", indexes, correct);
