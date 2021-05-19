9aed1a2c17df7e9e9ac0deb97ce7311f5badadcd
ryoha000 <the7uya@gmail.com>
2021-05-19 15:53:01 +0900

    add test of get_labels_by_scores

--- src/grid/label.rs
+++ src/grid/label.rs
@@ -47,3 +47,33 @@ pub fn get_labels_by_scores(scores: &[u32], grid: &utils::Grid, threshold: u32) 
     let contrours = utils::get_contrours_from_labels(&labels, &size);
     utils::GridLabel{ labels: labels, contrours: contrours }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn can_get_grid_labels() {
+        let scores = vec![
+             3, 10, 15,  6,  0,
+            16, 12, 13, 10,  3,
+            12, 16,  2,  5, 11,
+        ];
+        let grid = utils::Grid{ rows: 3, cols: 5 };
+        let threshold = 10;
+        let grid_labels = get_labels_by_scores(&scores, &grid, threshold);
+        let expected_labels = vec![
+            1, 2, 2, 1, 1,
+            2, 2, 2, 2, 1,
+            2, 2, 1, 1, 3,
+        ];
+        let expected_contrours = vec![
+            false, true, true, false, false,
+            true, false, true, true, false,
+            true, true, false, false, true,
+        ];
+        for i in 0..scores.len() {
+            assert_eq!(grid_labels.labels[i], expected_labels[i], "label is different. index: {}", i);
+            assert_eq!(grid_labels.contrours[i], expected_contrours[i], "contrours is different. index: {}", i);
+        }
+    }
+}
