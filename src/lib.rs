061efbf94421d6281701e177a9c58f7524eaa087
ryoha000 <the7uya@gmail.com>
2021-05-20 18:42:57 +0900

    add main method

--- src/lib.rs
+++ src/lib.rs
@@ -8,19 +8,27 @@ mod filter;
 mod grid;
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-
-fn get_labels_by_gray(gray: &[u8], width: u32, height: u32) -> Vec<u32> {
+pub fn get_labels_by_gray(gray: &[u8], width: u32, height: u32, side: u32) -> Box<[u32]> {
     let binary = crate::threshold::gray_to_binary(gray);
     let divided_binary = divide::get_divided_binary(&(width, height, &binary));
     let noise_filtered_binary = filter::get_filtered_binary(&divided_binary, width / 40);
-    let labels = grid::get_labels(width, height, &noise_filtered_binary);
-    labels
+    let labels = grid::get_labels(width, height, &noise_filtered_binary, side);
+    labels.into_boxed_slice()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils::test;
+    #[test]
+    fn can_get_labels_by_gray() {
+        let (width, height, gray) = test::load_gray_image();
+        let binary = threshold::gray_to_binary(&gray);
+        let divided_binary = divide::get_divided_binary(&(width, height, &binary));
+        let noise_filtered_binary = filter::get_filtered_binary(&divided_binary, width / 40);
+        let side = width / 50;
+        let labels = grid::get_labels(width, height, &noise_filtered_binary, side);
+        let grid = utils::Grid{ rows: (height as f32 / side as f32).ceil() as usize, cols: (width as f32 / side as f32).ceil() as usize };
+        test::get_visualized_labels(&labels, &(width, height, &noise_filtered_binary), side, &grid, &vec![0, 1]);
+    }
 }
